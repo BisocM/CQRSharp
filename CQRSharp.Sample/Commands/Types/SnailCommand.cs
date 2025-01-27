7495420b7759bc7560dcf0754bd89e4e3e773479
bisocm <mikhail.babunashvili@gmail.com>
2025-01-27 12:19:26 +0000

    Sample v1.0

---
+++ CQRSharp.Sample/Commands/Types/SnailCommand.cs
@@ -0,0 +1,10 @@
+﻿using CQRSharp.Interfaces.Markers.Command;
+using CQRSharp.Sample.Attributes.Menu;
+using CQRSharp.Sample.Context;
+using CQRSharp.Sample.Management.Menu;
+
+namespace CQRSharp.Sample.Commands.Types;
+
+//Position this command in the secondary menu.
+[MenuOrientation(MenuState.SecondaryMenu, 1)]
+public class SnailCommand : CommandBase<SampleRequestContext> { }
